43b9bc845e74e10f8ed93547da5d9277a142c1fc
shwzr <showzur@gmail.com>
2024-05-23 13:39:38 +0200

    first commit

--- logged.php
+++ logged.php
@@ -1,12 +1,15 @@
 <?php
-session_start(); // Démarre ou reprend une session
-require 'assets/php/request.php'; // Inclut le fichier de requête pour des opérations supplémentaires
+session_start();
+header('Content-Type: text/html; charset=UTF-8');
 
-// Redirige l'utilisateur vers la page de connexion si non connecté ou si la session/cookie est invalide
+// Vérifie si l'utilisateur est connecté
 if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true || !isset($_COOKIE[session_name()])) {
     header("Location: login.php");
     exit;
 }
+
+// Récupère le type de connexion depuis la session
+$connection_type = isset($_SESSION["connection_type"]) ? $_SESSION["connection_type"] : "Inconnu";
 ?>
 
 <!DOCTYPE html>
@@ -19,7 +22,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true || !isset($_
     <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
     <style>
         .logout-link:hover {
-            background-color: #005fa6; /* Modifie le fond au survol pour le lien de déconnexion */
+            background-color: #005fa6;
         }
     </style>
 </head>
@@ -33,7 +36,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true || !isset($_
     <ul>
         <li><a href="index.php">Accueil</a></li>
         <li><a href="logged.php">Profil</a></li>
-        <li><a href="teams.php">Équipes</a></li>
+        <li><a href="teams.php">Equipes</a></li>
     </ul>
 </nav>
 
@@ -42,9 +45,10 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true || !isset($_
     <div class="image-container">
         <img src="assets/img/user.png" alt="Image utilisateur" style="width: 80px; height: 80px; display: flex; margin: auto; margin-bottom: 50px; border: 3px solid #0077cc; border-radius: 50%; padding: 10px;">
     </div>
-    <p><strong>Nom d'utilisateur :</strong> <?php echo htmlspecialchars($_SESSION["username"]); // Affiche le nom d'utilisateur en sécurité ?></p>
+    <p><strong>Nom d'utilisateur :</strong> <?php echo htmlspecialchars($_SESSION["username"], ENT_QUOTES, 'UTF-8'); // Affiche le nom d'utilisateur en sécurité ?></p>
+    <p><strong>Connexion :</strong> <?php echo "Connexion " . htmlspecialchars($connection_type, ENT_QUOTES, 'UTF-8') . " reussie."; // Affiche le type de connexion ?></p>
     <div style="text-align: center; margin-top: 20px;">
-        <a href="logout.php" class="logout-link">Déconnexion</a> <!-- Lien de déconnexion -->
+        <a href="logout.php" class="logout-link">Deconnexion</a>
     </div>
 </div>
 
