185e275b503785050e94be08a8c033f2a60598e8
shwzr <showzur@gmail.com>
2024-05-27 15:36:08 +0200

    first commit

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // Configuration de LDAP
-$ldap_host = 'shyno.tech';  // Adresse IP du serveur LDAP
+$ldap_host = '192.168.1.100';  // Adresse IP du serveur LDAP
 $ldap_port = '389';  // Port LDAP
 $ldaps_port = '636'; // Port LDAPS
 $ldap_domain = 'shyno.tech';  // Domaine LDAP
